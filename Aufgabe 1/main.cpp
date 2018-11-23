96193e9c511b41a36d33f0bb274ecf4c486aeb95
Suayb Yurdakul <suaybyurdakul@live.de>
2018-11-23 11:12:20 +0100

    Conform

--- Aufgabe 1/main.cpp
+++ Aufgabe 1/main.cpp
@@ -1,20 +1,57 @@
+//// 6 Zahlen zwischen 1 und 79 einlesen und als Sternchen (*) ausgeben
+//// Autor: Suayb Yurdakul
+//// Datum: 2018-11-13
+//
+//#include <iostream>
+//#include <array>
+//using namespace std;
+//
+//int main() {
+//    const int arraySize = 6;
+//    array<int, arraySize> input;
+//    int check;
+//
+//    cout << "Programm zum Einlesen von " << arraySize << " Zahlen zwischen 1 und 79 und entsprechende Ausgabe in //Sternchen (*)\n\n";
+//
+//    for (int i = 0; i < input.size(); i++) {
+//        cout << (i + 1) << ". Zahl zwischen 1 und 79: ";
+//        cin >> check;
+//
+//        if (check < 1 || check > 79) {
+//            cout << "Ihre Zahl liegt nicht zwischen 1 und 79!\n";
+//            i--;
+//        }
+//        else {
+//            input[i] = check;
+//        }
+//    }
+//
+//    cout << "\n";
+//
+//    for (int i = 0; i < input.size(); i++) {
+//        for (int j = 0; j < input[i]; j++) {
+//            cout << "*";
+//        }
+//        cout << "\n";
+//    }
+//
+//    return 0;
+//}
+
 // 6 Zahlen zwischen 1 und 79 einlesen und als Sternchen (*) ausgeben
 // Autor: Suayb Yurdakul
 // Datum: 2018-11-13
 
 #include <iostream>
-#include <array>
 using namespace std;
 
 int main() {
-    const int arraySize = 6;
-    array<int, arraySize> input;
-    int check;
+    int in0, in1, in2, in3, in4, in5, check, out;
 
-    cout << "Programm zum Einlesen von " << arraySize << " Zahlen zwischen 1 und 79 und entsprechende Ausgabe in Sternchen (*)\n\n";
+    cout << "Programm zum Einlesen von 6 Zahlen zwischen 1 und 79 und entsprechende Ausgabe in Sternchen (*)\n\n";
 
-    for (int i = 0; i < input.size(); i++) {
-        cout << (i + 1) << ". Zahl zwischen 1 und 79: ";
+    for (int i = 0; i < 6; i++) {
+        cout << "Geben Sie die " << (i + 1) << ". Zahl zwischen 1 und 79 ein:\n";
         cin >> check;
 
         if (check < 1 || check > 79) {
@@ -22,14 +59,60 @@ int main() {
             i--;
         }
         else {
-            input[i] = check;
+            switch (i) {
+            case 0:
+                in0 = check;
+                break;
+            case 1:
+                in1 = check;
+                break;
+            case 2:
+                in2 = check;
+                break;
+            case 3:
+                in3 = check;
+                break;
+            case 4:
+                in4 = check;
+                break;
+            case 5:
+                in5 = check;
+                break;
+            default:
+                cout << "Big Y I K E S ...";
+                break;
+            }
         }
     }
 
     cout << "\n";
 
-    for (int i = 0; i < input.size(); i++) {
-        for (int j = 0; j < input[i]; j++) {
+    for (int i = 0; i < 6; i++) {
+        switch (i) {
+        case 0:
+            out = in0;
+            break;
+        case 1:
+            out = in1;
+            break;
+        case 2:
+            out = in2;
+            break;
+        case 3:
+            out = in3;
+            break;
+        case 4:
+            out = in4;
+            break;
+        case 5:
+            out = in5;
+            break;
+        default:
+            cout << "Bigger Y I K E S ...";
+            break;
+        }
+
+        for (int j = 0; j < out; j++) {
             cout << "*";
         }
         cout << "\n";
--- Aufgabe 4/main.cpp
+++ Aufgabe 4/main.cpp
@@ -1,15 +1,89 @@
+//// Simulation: 2 Würfel 6.000.000 mal werfen und Häufigkeitstabelle ausgeben
+//// Autor: Suayb Yurdakul
+//// Datum: 2018-11-13
+//
+//#include <iostream>
+//#include <array>
+//#include <cstdlib>
+//using namespace std;
+//
+//int main() {
+//    const int sumDice = 11, maxRolls = 6000000;
+//    array<int, sumDice> sumEyes = { 0 };
+//
+//    for (int i = 0; i < maxRolls; i++) {
+//        int currentRoll = (rand() % 6 + 1) + (rand() % 6 + 1);
+//
+//        switch (currentRoll) {
+//        case 1:
+//            break;
+//        case 2:
+//            sumEyes[0]++;
+//            break;
+//        case 3:
+//            sumEyes[1]++;
+//            break;
+//        case 4:
+//            sumEyes[2]++;
+//            break;
+//        case 5:
+//            sumEyes[3]++;
+//            break;
+//        case 6:
+//            sumEyes[4]++;
+//            break;
+//        case 7:
+//            sumEyes[5]++;
+//            break;
+//        case 8:
+//            sumEyes[6]++;
+//            break;
+//        case 9:
+//            sumEyes[7]++;
+//            break;
+//        case 10:
+//            sumEyes[8]++;
+//            break;
+//        case 11:
+//            sumEyes[9]++;
+//            break;
+//        case 12:
+//            sumEyes[10]++;
+//            break;
+//        default:
+//            cout << "Big Y I K E R S ...\n";
+//        }
+//    }
+//
+//    cout << "Augensumme\tAnzahl gewürfelt\n";
+//    for (int i = 0; i < sumEyes.size(); i++) {
+//        cout << (i + 2) << "\t\t" << sumEyes[i] << "\n";
+//    }
+//
+//    return 0;
+//}
+
 // Simulation: 2 Würfel 6.000.000 mal werfen und Häufigkeitstabelle ausgeben
 // Autor: Suayb Yurdakul
 // Datum: 2018-11-13
 
 #include <iostream>
-#include <array>
 #include <cstdlib>
 using namespace std;
 
 int main() {
-    const int sumDice = 11, maxRolls = 6000000;
-    array<int, sumDice> sumEyes = { 0 };
+    const int maxRolls = 6000000;
+    int sd00 = 0, 
+        sd01 = 0, 
+        sd02 = 0, 
+        sd03 = 0, 
+        sd04 = 0, 
+        sd05 = 0, 
+        sd06 = 0, 
+        sd07 = 0, 
+        sd08 = 0, 
+        sd09 = 0, 
+        sd10 = 0;
 
     for (int i = 0; i < maxRolls; i++) {
         int currentRoll = (rand() % 6 + 1) + (rand() % 6 + 1);
@@ -18,37 +92,37 @@ int main() {
         case 1:
             break;
         case 2:
-            sumEyes[0]++;
+            sd00++;
             break;
         case 3:
-            sumEyes[1]++;
+            sd01++;
             break;
         case 4:
-            sumEyes[2]++;
+            sd02++;
             break;
         case 5:
-            sumEyes[3]++;
+            sd03++;
             break;
         case 6:
-            sumEyes[4]++;
+            sd04++;
             break;
         case 7:
-            sumEyes[5]++;
+            sd05++;
             break;
         case 8:
-            sumEyes[6]++;
+            sd06++;
             break;
         case 9:
-            sumEyes[7]++;
+            sd07++;
             break;
         case 10:
-            sumEyes[8]++;
+            sd08++;
             break;
         case 11:
-            sumEyes[9]++;
+            sd09++;
             break;
         case 12:
-            sumEyes[10]++;
+            sd10++;
             break;
         default:
             cout << "Big Y I K E R S ...\n";
@@ -56,9 +130,17 @@ int main() {
     }
 
     cout << "Augensumme\tAnzahl gewürfelt\n";
-    for (int i = 0; i < sumEyes.size(); i++) {
-        cout << (i + 2) << "\t\t" << sumEyes[i] << "\n";
-    }
+    cout << 2 << "\t\t" << sd00 << "\n";
+    cout << 3 << "\t\t" << sd01 << "\n";
+    cout << 4 << "\t\t" << sd02 << "\n";
+    cout << 5 << "\t\t" << sd03 << "\n";
+    cout << 6 << "\t\t" << sd04 << "\n";
+    cout << 7 << "\t\t" << sd05 << "\n";
+    cout << 8 << "\t\t" << sd06 << "\n";
+    cout << 9 << "\t\t" << sd07 << "\n";
+    cout << 10 << "\t\t" << sd08 << "\n";
+    cout << 11 << "\t\t" << sd09 << "\n";
+    cout << 12 << "\t\t" << sd10 << "\n";
 
     return 0;
 }
